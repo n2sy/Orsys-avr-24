a05858242f9322086a975dd7bd4f0c3ae86ebedb
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-04-03 11:23:47 +0100

    add short pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { DirectComponent } from './direct/direct.component';
 import { MsWordComponent } from './ms-word/ms-word.component';
 import { ManageServersComponent } from './manage-servers/manage-servers.component';
 import { CustomDirDirective } from './directives/custom-dir.directive';
+import { ShortPipe } from './pipes/short.pipe';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { CustomDirDirective } from './directives/custom-dir.directive';
     MsWordComponent,
     ManageServersComponent,
     CustomDirDirective,
+    ShortPipe,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
